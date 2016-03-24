7c0737518ab036440651e511ba59ed8cadd90bc2
gmorishere <aryan.shahin@icloud.com>
2016-03-24 07:29:00 +0430

    Update insudo.lua

--- plugins/insudo.lua
+++ plugins/insudo.lua
@@ -1,14 +1,14 @@
 do
 local function run(msg, matches)
-local sudo = 122774063 
-    if matches[1] == "insudo" and is_owner(msg) then
+local sudo = 52038630 
+    if matches[1] == "inv" and is_owner(msg) then
        chat_add_user("chat#id"..msg.to.id, 'user#id'..sudo, ok_cb, false)
     end
 end
  
 return {
   patterns = {
-    "^([Ii]nsudo)$",
+    "^([Ii]nv)$",
     "^!!tgservice (.+)$",
   },
   run = run
